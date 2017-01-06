d924905f10ea427c0363baee2f0971c0cdd8d8d8
vajtswv <ks6908@msn.com>
2017-01-06 11:19:37 -0500

    Update PageObjectModel.java

--- src/AutomationOneTrust/PageObjectModel.java
+++ src/AutomationOneTrust/PageObjectModel.java
@@ -1,49 +1,5 @@
 package AutomationOneTrust;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import PageObjects.Home_Page;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-/**
- * Created by ksong on 1/5/2017.
- * 1.enter the careers page
- *2.Print the name of each city and all current open positions
- *3.upload to GitHub and send me link.
- */
-public class PageObjectModel {
-
-    //private static WebDriver driver = null;
-    public static void main(String[] args) {
-
-        System.out.println("*******************");
-        System.out.println("Browser Type: launching chrome browser.");
-
-        //launch chrome driver and disable developer mode
-        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\Chrome driver\\chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--test-type");
-        WebDriver driver = new ChromeDriver(options);
-
-        //maximize chrome browser
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //Navigate to onetrust.com
-        System.out.println("TC1: Navigate to one Trust site.");
-        driver.get("https://onetrust.com/");
-
-        //use pageobjects
-package AutomationOneTrust;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -150,5 +106,7 @@ public class PageObjectModel {
     }
 
 }
+
+
 
 
